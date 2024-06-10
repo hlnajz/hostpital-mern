4b62321d0cc1302916fe10921a577a5c13ead624
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2024-06-10 16:01:54 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,3 @@
-// import express from "express";
-// import { dbConnection } from "./database/dbConnection.js";
-// import { config } from "dotenv";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import fileUpload from "express-fileupload";
-// import { errorMiddleware } from "./middlewares/error.js";
-// import messageRouter from "./router/messageRouter.js";
-// import userRouter from "./router/userRouter.js";
-// import appointmentRouter from "./router/appointmentRouter.js";
-
-// const app = express();
-// config({ path: "./config/config.env" });
-
-// app.use(
-//   cors({
-//     origin: "https://hostpital-mern-frontend.vercel.app/",
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
-
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   fileUpload({
-//     useTempFiles: true,
-//     tempFileDir: "/tmp/",
-//   })
-// );
-// app.use("/api/v1/message", messageRouter);
-// app.use("/api/v1/user", userRouter);
-// app.use("/api/v1/appointment", appointmentRouter);
-
-// dbConnection();
-
-// app.use(errorMiddleware);
-// export default app;
-
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import { config } from "dotenv";
@@ -64,6 +23,20 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://hostpital-mern-frontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(
   fileUpload({
