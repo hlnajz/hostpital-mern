e15130ef99d5acc3fded7e5497b6a5b35fa16d87
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2024-06-10 15:33:24 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://hostpital-mern.vercel.app/api/v1/user/patient/me",
+          "https://hostpital-mern-backend.vercel.app/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
