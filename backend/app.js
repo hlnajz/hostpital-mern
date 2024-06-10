c52313fee29ef68c877c5a108bad4200b2e6ded4
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2024-06-10 01:18:54 +0100

    add config vercel

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    origin: ["https://hostpital-mern.vercel.app"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
