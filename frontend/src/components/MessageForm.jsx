b2bd4ae7063e210518a866fff62742babe35af9a
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2024-06-10 01:58:05 +0100

    Update MessageForm.jsx

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -14,7 +14,7 @@ const MessageForm = () => {
     try {
       await axios
         .post(
-          "https://hostpital-mern-frontend.vercel.app/api/v1/message/send",
+          "https://hostpital-mern.vercel.app/api/v1/message/send",
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
